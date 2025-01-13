03bf321a8a6a4e95120f9eeb86010c60f7f2a505
Edugonzalez <eduu.gonzalez@gmail.com>
2025-01-13 14:08:21 +0100

    completado

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,22 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
-
+    Queue<String> queu = new LinkedList<>();
+    queu.add("Smith");
+    queu.add("Montessori");
+    queu.add("Peralta");
+    queu.add("House");
+    return queu;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        while (!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
     }
 
     public static void main(String[] args) {
-
+        Queue<String> newQueue = createQueue();
+        printAndEmptyQueue(newQueue);
     }
 
 }
